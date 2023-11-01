9a329bccff3bfa4b3fec4cd805ac1df5a533b366
SamvelMhitaryan <mhitaryan.fortech@gmail.com>
2023-11-01 11:51:10 +0300

    review 1: add custom error, fix type hints, add docstrings.

--- task1/BaseRequest.py
+++ task1/BaseRequest.py
@@ -1,5 +1,9 @@
 from enum import Enum
+from typing import Optional, Any
 import re
+
+class ValidationError(Exception):
+    pass
 
 class EnumRequest(Enum):
     GET = 'GET'
@@ -8,24 +12,27 @@ class EnumRequest(Enum):
     PATCH = 'PATCH'
 
 class BaseRequest:
-    def __init__(self, url: str, method: EnumRequest, params: dict=None, body: dict=None):
+    def __init__(self, url: str, method: EnumRequest, params: Optional[dict[str, Any]] = None, body: Optional[dict[str, Any]] = None):
         self.url = self.validate_url(url)
         self.method = method
         self.params = params
         self.body = self.validate_body(body)
 
-    def validate_body(self, body: dict = None): #проверяем валидность условия body 
+    def validate_body(self, body: dict = None): 
+        """валидация первого условия задачи"""
         if self.method != EnumRequest.POST and body is not None:
             raise Exception('method must be POST or body must be not None')
         return body
 
-    def validate_url(self, url): #проверяем валидность url 
+    def validate_url(self, url): 
+        """проверяем валидность url"""
         pattern = r'^https?://[a-zA-Z0-9.-]+(/[\S]*)?$'
-        if not re.match(pattern, url):
-            raise Exception('pattern is not true')
+        if not re.match(pattern, url): 
+            raise ValidationError('pattern is not true')
         return url
 
-    def validate_get_post(self, method, url): #вторая часть задачи
+    def validate_get_post(self, method, url):
+        """вторая часть задачи"""
         without_body = r'^https?://[a-zA-Z0-9.-]'
         with_body = r'^https?://[a-zA-Z0-9.-]+(/[\S]*)?$'
         if method == EnumRequest.GET and re.match(without_body, url):
@@ -33,9 +40,10 @@ class BaseRequest:
         elif method == EnumRequest.POST and re.match(with_body, url):
             return url 
         else:
-            raise Exception('conditions are not right')
+            raise ValidationError('conditions are not right')
 
-class Request(BaseRequest): #проверка кол-ва параметров
+class Request(BaseRequest):
+    """проверка кол-ва параметров"""
     MAX_PARAMS = 5
 
     def validate_params(self, params: dict=None): 
